ede66cd0b22be17d0ec068b234d6ffa692678af5
Manas <img_2022037@iiitm.ac.in>
2024-02-12 23:29:35 +0530

    Refactor MaxSubArraySum.cpp

--- general/MaxSubArraySum.cpp
+++ general/MaxSubArraySum.cpp
@@ -1,17 +1,45 @@
-class Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define pb push_back
+#define vi vector<int>
+#define f(i, s, e) for (int i = s; i < e; i++)
+#define cf(i, s, e) for (int i = s; i <= e; i++)
+#define rf(i, e, s) for (int i = e - 1; i >= s; i--)
+#define all(v) v.begin(), v.end()
+
+void solve()
 {
-public:
-    int maxSubArray(vector<int> &nums)
+    // TODO: Implement your solution here
+}
+int maxSubArray(vector<int> &nums)
+{
+    int maxSum = nums[0];
+    int currentSum = nums[0];
+
+    for (int i = 1; i < nums.size(); i++)
     {
-        int maxSum = nums[0];
-        int currentSum = nums[0];
-
-        for (int i = 1; i < nums.size(); i++)
-        {
-            currentSum = max(0, currentSum + nums[i]);
-            maxSum = max(maxSum, currentSum);
-        }
-
-        return maxSum;
+        currentSum = max(0, currentSum + nums[i]);
+        maxSum = max(maxSum, currentSum);
     }
-};
+
+    return maxSum;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
